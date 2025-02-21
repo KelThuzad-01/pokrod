aa77534f92e1e920a7eddb0d982404b6bc8e0658
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-02-22 00:18:28 +0100

    Update read_trainer_party.asm

--- engine/battle/read_trainer_party.asm
+++ engine/battle/read_trainer_party.asm
@@ -6,6 +6,8 @@ ReadTrainer:
 	ret nz
 
 ; set [wEnemyPartyCount] to 0, [wEnemyPartySpecies] to FF
+; XXX first is total enemy pokemon?
+; XXX second is species of first pokemon?
 	ld hl, wEnemyPartyCount
 	xor a
 	ld [hli], a
@@ -14,7 +16,7 @@ ReadTrainer:
 
 ; get the pointer to trainer data for this class
 	ld a, [wCurOpponent]
-	sub OPP_ID_OFFSET + 1 ; convert value from Pokémon to trainer
+	sub OPP_ID_OFFSET + 1 ; convert value from pokemon to trainer
 	add a
 	ld hl, TrainerDataPointers
 	ld c, a
@@ -25,9 +27,10 @@ ReadTrainer:
 	ld l, a
 	ld a, [wTrainerNo]
 	ld b, a
-
 ; At this point b contains the trainer number,
 ; and hl points to the trainer class.
+; Our next task is to iterate through the trainers,
+; decrementing b each time, until we get to the right one.
 .outer
 	dec b
 	jr z, .IterateTrainer
@@ -37,60 +40,34 @@ ReadTrainer:
 	jr nz, .inner
 	jr .outer
 
-; If the first byte of trainer data is FF,
-; - Each Pokémon has a specific level
-; - If [wLoneAttackNo] != 0, one Pokémon has un ataque especial
-; Otherwise, the first byte is the level of all Pokémon
+; if the first byte of trainer data is FF,
+; - each pokemon has a specific level
+;      (as opposed to the whole team being of the same level)
+; - if [wLoneAttackNo] != 0, one pokemon on the team has a special move
+; else the first byte is the level of every pokemon on the team
 .IterateTrainer
 	ld a, [hli]
-	cp $FF ; Is the trainer special?
-	jr z, .SpecialTrainer ; If so, check for special moves
+	cp $FF ; is the trainer special?
+	jr z, .SpecialTrainer ; if so, check for special moves
 	ld [wCurEnemyLevel], a
 .LoopTrainerData
-	call Random      ; Generar un número aleatorio
-    and %11         ; Limitar el número a 0-3 (4 opciones)
-
-    cp 0
-    jr z, .PickMagikarp
-    cp 1
-    jr z, .PickSnorlax
-    cp 2
-    jr z, .PickSquirtle
-    cp 3
-    jr z, .PickCharmander
-
-.PickMagikarp
-    ld a, MAGIKARP
-    jr .AssignPokemon
-
-.PickSnorlax
-    ld a, SNORLAX
-    jr .AssignPokemon
-
-.PickSquirtle
-    ld a, SQUIRTLE
-    jr .AssignPokemon
-
-.PickCharmander
-    ld a, CHARMANDER
-
-.AssignPokemon
-    ld [wCurPartySpecies], a
-    ld a, [wCurEnemyLevel]  ; Nivel base definido por el entrenador
-    ld [wCurEnemyLevel], a
-    ld a, ENEMY_PARTY_DATA
-    ld [wMonDataLocation], a
-    push hl
-    call AddPartyMon
-    pop hl
-    jr .LoopTrainerData
-
-.SpecialTrainer
-; If this code is being run:
-; - Each Pokémon has a specific level
-; - If [wLoneAttackNo] != 0, one Pokémon has un ataque especial
 	ld a, [hli]
-	and a ; Have we reached the end of the trainer data?
+	and a ; have we reached the end of the trainer data?
+	jr z, .FinishUp
+	ld [wCurPartySpecies], a
+	ld a, ENEMY_PARTY_DATA
+	ld [wMonDataLocation], a
+	push hl
+	call AddPartyMon
+	pop hl
+	jr .LoopTrainerData
+.SpecialTrainer
+; if this code is being run:
+; - each pokemon has a specific level
+;      (as opposed to the whole team being of the same level)
+; - if [wLoneAttackNo] != 0, one pokemon on the team has a special move
+	ld a, [hli]
+	and a ; have we reached the end of the trainer data?
 	jr z, .AddLoneMove
 	ld [wCurEnemyLevel], a
 	ld a, [hli]
@@ -101,10 +78,9 @@ ReadTrainer:
 	call AddPartyMon
 	pop hl
 	jr .SpecialTrainer
-
 .AddLoneMove
-; Does the trainer have a single monster with a different move?
-	ld a, [wLoneAttackNo] ; Brock is 01, Misty is 02, Erika es 04, etc.
+; does the trainer have a single monster with a different move?
+	ld a, [wLoneAttackNo] ; Brock is 01, Misty is 02, Erika is 04, etc
 	and a
 	jr z, .AddTeamMove
 	dec a
@@ -120,44 +96,40 @@ ReadTrainer:
 	call AddNTimes
 	ld [hl], d
 	jr .FinishUp
-
 .AddTeamMove
-; Check if our trainer's team has special moves
+; check if our trainer's team has special moves
 
-; Get trainer class number
+; get trainer class number
 	ld a, [wCurOpponent]
 	sub OPP_ID_OFFSET
 	ld b, a
 	ld hl, TeamMoves
 
-; Iterate through entries in TeamMoves, checking each for our trainer class
+; iterate through entries in TeamMoves, checking each for our trainer class
 .IterateTeamMoves
 	ld a, [hli]
 	cp b
-	jr z, .GiveTeamMoves ; Is there a match?
-	inc hl ; If not, go to the next entry
+	jr z, .GiveTeamMoves ; is there a match?
+	inc hl ; if not, go to the next entry
 	inc a
 	jr nz, .IterateTeamMoves
 
-; No matches found. Is this trainer champion rival?
+; no matches found. is this trainer champion rival?
 	ld a, b
 	cp RIVAL3
 	jr z, .ChampionRival
-	jr .FinishUp ; Nope
-
+	jr .FinishUp ; nope
 .GiveTeamMoves
 	ld a, [hl]
 	ld [wEnemyMon5Moves + 2], a
 	jr .FinishUp
+.ChampionRival ; give moves to his team
 
-.ChampionRival
-; Give moves to his team
-
-; Pidgeot
+; pidgeot
 	ld a, SKY_ATTACK
 	ld [wEnemyMon1Moves + 2], a
 
-; Starter
+; starter
 	ld a, [wRivalStarter]
 	cp STARTER3
 	ld b, MEGA_DRAIN
@@ -165,13 +137,12 @@ ReadTrainer:
 	cp STARTER1
 	ld b, FIRE_BLAST
 	jr z, .GiveStarterMove
-	ld b, BLIZZARD ; Must be Squirtle
+	ld b, BLIZZARD ; must be squirtle
 .GiveStarterMove
 	ld a, b
 	ld [wEnemyMon6Moves + 2], a
-
 .FinishUp
-; Clear wAmountMoneyWon addresses
+; clear wAmountMoneyWon addresses
 	xor a
 	ld de, wAmountMoneyWon
 	ld [de], a
@@ -181,9 +152,8 @@ ReadTrainer:
 	ld [de], a
 	ld a, [wCurEnemyLevel]
 	ld b, a
-
 .LastLoop
-; Update wAmountMoneyWon addresses (money to win) based on enemy's level
+; update wAmountMoneyWon addresses (money to win) based on enemy's level
 	ld hl, wTrainerBaseMoney + 1
 	ld c, 2 ; wAmountMoneyWon is a 3-byte number
 	push bc
@@ -192,5 +162,5 @@ ReadTrainer:
 	inc de
 	inc de
 	dec b
-	jr nz, .LastLoop ; Repeat wCurEnemyLevel times
+	jr nz, .LastLoop ; repeat wCurEnemyLevel times
 	ret
