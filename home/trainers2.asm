802ab9783f82e268af999227b98ea4f9f8032a78
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-02-21 22:19:29 +0100

    Update trainers2.asm

--- home/trainers2.asm
+++ home/trainers2.asm
@@ -1,39 +1,21 @@
-CheckRocketConversion:
-    call Random        
-    cp 26             ; 10% de probabilidad
-    jr nc, .KeepTrainer ; Si no se cumple, mantenemos el entrenador original
-
-    ld a, OPP_ROCKET_GRUNT1  ; Cambia al entrenador a Rocket
-    ld [wTrainerClass], a
-
-    ld a, SPRITE_ROCKET ; Cambia el sprite en el mapa
-    ld [wCurOpponentSprite], a
-
-    jr .Continue
-
-.KeepTrainer
-    ; Mantener el tipo de entrenador y sprite original
-
-.Continue
-
 GetTrainerInformation::
-	call CheckRocketConversion  ; Aplica la probabilidad de ser Rocket
 	call GetTrainerName
 	ld a, [wLinkState]
 	and a
 	jr nz, .linkBattle
 	ld a, BANK(TrainerPicAndMoneyPointers)
 	call BankswitchHome
-	ld a, [wTrainerClass]  ; Ahora puede ser Rocket con un 10% de probabilidad
+	ld a, [wTrainerClass]
 	dec a
 	ld hl, TrainerPicAndMoneyPointers
 	ld bc, $5
 	call AddNTimes
-
-	; Actualizar sprite en el mapa
-	ld a, [wCurOpponentSprite]
-	ld [wTrainerPicPointer], a
-
+	ld de, wTrainerPicPointer
+	ld a, [hli]
+	ld [de], a
+	inc de
+	ld a, [hli]
+	ld [de], a
 	ld de, wTrainerBaseMoney
 	ld a, [hli]
 	ld [de], a
@@ -48,7 +30,6 @@ GetTrainerInformation::
 	inc hl
 	ld [hl], d
 	ret
-
 
 GetTrainerName::
 	farjp GetTrainerName_
