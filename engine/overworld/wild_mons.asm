680a20717eedc0d9cb66fd799abeae705077486d
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-02-21 15:44:15 +0100

    Update wild_mons.asm
    avoid wild Pokemon

--- engine/overworld/wild_mons.asm
+++ engine/overworld/wild_mons.asm
@@ -10,10 +10,10 @@ LoadWildData::
 	ld a, [hli]
 	ld h, [hl]
 	ld l, a       ; hl now points to wild data for current map
-	ld a, [hli]
-	ld [wGrassRate], a
-	and a
-	jr z, .NoGrassData ; if no grass data, skip to surfing data
+	xor a              ; Establece a = 0 (ningún encuentro en hierba)
+	ld [wGrassRate], a ; La tasa de encuentros en la hierba ahora es 0
+	jr .NoGrassData    ; Salta directamente a la parte de agua
+
 	push hl
 	ld de, wGrassMons ; otherwise, load grass data
 	ld bc, $14
@@ -22,12 +22,8 @@ LoadWildData::
 	ld bc, $14
 	add hl, bc
 .NoGrassData
-	ld a, [hli]
-	ld [wWaterRate], a
-	and a
-	ret z        ; if no water data, we're done
-	ld de, wWaterMons  ; otherwise, load surfing data
-	ld bc, $14
-	jp CopyData
+	xor a              ; Establece a = 0 (ningún encuentro al surfear)
+	ld [wWaterRate], a ; La tasa de encuentros en agua ahora es 0
+	ret               ; Sale de la función sin cargar datos de Surfing
 
 INCLUDE "data/wild/grass_water.asm"
