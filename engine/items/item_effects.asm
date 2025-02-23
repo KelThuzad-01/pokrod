e4f0cc18dd4e22a75d664e2bef1c95a3efd59ec5
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-02-23 10:29:56 +0100

    Update item_effects.asm

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -93,6 +93,7 @@ ItemUsePtrTable:
 	dw UnusableItem      ; LIFT_KEY
 	dw UnusableItem      ; EXP_ALL
 	dw ItemUseOldRod     ; OLD_ROD
+	dw ItemUseGoodRod    ; GOOD_ROD
 	dw ItemUseSuperRod   ; SUPER_ROD
 	dw ItemUsePPUp       ; PP_UP
 	dw ItemUsePPRestore  ; ETHER
@@ -1828,6 +1829,33 @@ ItemUseOldRod:
 	lb bc, 5, MAGIKARP
 	ld a, $1 ; set bite
 	jp RodResponse
+ItemUseGoodRod:
+	call FishingInit
+	jp c, ItemUseNotTime
+.RandomLoop
+	call Random
+	srl a
+	jr c, .SetBite
+	and %11
+	cp 2
+	jr nc, .RandomLoop
+	; choose which monster appears
+	ld hl, GoodRodMons
+	add a
+	ld c, a
+	ld b, 0
+	add hl, bc
+	ld b, [hl]
+	inc hl
+	ld c, [hl]
+	and a
+.SetBite
+	ld a, 0
+	rla
+	xor 1
+	jr RodResponse
+
+INCLUDE "data/wild/good_rod.asm"
 
 ItemUseSuperRod:
 	call FishingInit
