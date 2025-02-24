0721bad6ba3d38118f3eb3b7cc76bc2643f8f7c5
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-02-24 18:49:05 +0100

    Update Route1.asm

--- data/maps/headers/Route1.asm
+++ data/maps/headers/Route1.asm
@@ -1,4 +1,4 @@
 	map_header Route1, ROUTE_1, OVERWORLD, NORTH | SOUTH
 	connection north, ViridianCity, VIRIDIAN_CITY, -5
-	connection south, Route12, ROUTE_12, 0
+	connection south, Route12, ROUTE_12, 1
 	end_map_header
