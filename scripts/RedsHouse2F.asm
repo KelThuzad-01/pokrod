6f16de92a53c077535025729919ddb6d0a498b0a
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-02-26 17:07:08 +0100

    Update RedsHouse2F.asm

--- scripts/RedsHouse2F.asm
+++ scripts/RedsHouse2F.asm
@@ -3,16 +3,21 @@ RedsHouse2F_Script:
 	ld hl, RedsHouse2F_ScriptPointers
 	ld a, [wRedsHouse2FCurScript]
 	jp CallFunctionInTable
-    
-; Verificar si ya se entregó Lapras
-    ld a, [wStatusFlags4]
-    bit BIT_GOT_LAPRAS, a
-    ret nz  ; Si ya se entregó, no hacer nada
-    lb bc, GYARADOS, 15   ; Especificar Lapras nivel 15
-    call GivePokemon    ; Entregar el Pokémon
-    ld hl, wStatusFlags4
-    set BIT_GOT_LAPRAS, [hl]
-    ret
+
+	; Verificar si ya se entregó Lapras
+	ld a, [wStatusFlags4]
+	bit BIT_GOT_LAPRAS, a
+	ret nz  ; Si ya se entregó, no hacer nada
+	
+	; Entregar Lapras automáticamente
+	lb bc, LAPRAS, 15   ; Especificar Lapras nivel 15
+	call GivePokemon    ; Entregar el Pokémon y mostrar pantalla de mote
+	
+	; Marcar Lapras como entregado
+	ld hl, wStatusFlags4
+	set BIT_GOT_LAPRAS, [hl]
+	
+	ret
 
 RedsHouse2F_ScriptPointers:
 	def_script_pointers
