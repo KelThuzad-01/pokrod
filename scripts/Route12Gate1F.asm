aaeeabedca0455de09c05fddc245d13b430263b7
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-02-26 09:39:50 +0100

    Update Route12Gate1F.asm

--- scripts/Route12Gate1F.asm
+++ scripts/Route12Gate1F.asm
@@ -8,27 +8,20 @@ Route12Gate1F_Script:
     bit BIT_GOT_LAPRAS, a
     ret nz  ; Si ya se entregó, no hacer nada
 
-    ; Guardar configuración actual
-    ld a, [wOptions]
-    push af                 ; Guardamos la configuración original
-
-    ; Desactivar la opción de mote
-    res 6, a                ; Borra el bit que habilita la pregunta de mote
-    ld [wOptions], a        ; Guardamos la configuración temporal
+    ; Simular pulsación del botón B para cerrar el mote
+    ld a, $02  ; Código del botón B
+    ldh [hJoyHeld], a  ; Simula que el jugador presionó B
 
     ; Dar Lapras automáticamente
-    lb bc, LAPRAS, 20       ; Especificar Lapras nivel 20
-    call GivePokemon        ; Entregar el Pokémon
-
-    ; Restaurar configuración original
-    pop af
-    ld [wOptions], a        ; Restauramos el estado anterior de las opciones
+    lb bc, LAPRAS, 15   ; Especificar Lapras nivel 15
+    call GivePokemon    ; Entregar el Pokémon
 
     ; Marcar Lapras como entregado
     ld hl, wStatusFlags4
     set BIT_GOT_LAPRAS, [hl]
 
     ret
+
 
 Route12Gate1FGuardText:
     text "¡Bienvenido!"
