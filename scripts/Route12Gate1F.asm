986c8332b599dca562fea94b82aa4c6ae3c35204
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-03-01 21:44:58 +0100

    Update Route12Gate1F.asm

--- scripts/Route12Gate1F.asm
+++ scripts/Route12Gate1F.asm
@@ -4,7 +4,7 @@ Route12Gate1F_Script:
 Route12Gate1F_TextPointers:
 	def_text_pointers
 	dw_const Route12Gate1FGuardText, TEXT_ROUTE12GATE1F_GUARD
-    dw_const Route12Gate1FPokeballText, TEXT_ROUTE12_GATE1F_POKEMON_POKEBALL
+        dw_const Route12Gate1FPokeballText, TEXT_ROUTE12_GATE1F_POKEMON_POKEBALL
 
 Route12Gate1FGuardText:
 	text_far _Route12Gate1FGuardText
