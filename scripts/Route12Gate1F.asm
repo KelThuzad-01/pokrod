7d0b495ae358c80df3549b7817731b29a413e39b
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-03-02 00:11:41 +0100

    Update Route12Gate1F.asm

--- scripts/Route12Gate1F.asm
+++ scripts/Route12Gate1F.asm
@@ -4,7 +4,7 @@ Route12Gate1F_Script:
 Route12Gate1F_TextPointers:
 	def_text_pointers
 	dw_const Route12Gate1FGuardText, TEXT_ROUTE12GATE1F_GUARD
-        dw_const Route12Gate1FPokeballText, TEXT_ROUTE12_GATE1F_POKEMON_POKEBALL
+	dw_const Route12Gate1FPokeballText, TEXT_ROUTE12_GATE1F_POKEMON_POKEBALL
 
 Route12Gate1FGuardText:
 	text_far _Route12Gate1FGuardText
@@ -12,12 +12,23 @@ Route12Gate1FGuardText:
 
 Route12Gate1FPokeballText:
 	text_asm
+	CheckEvent EVENT_GOT_ROUTE12_GYARADOS  ; Verificar si ya se recogió el Pokémon
+	jr nz, .already_got_it                 ; Si ya lo tiene, salir
+
 	lb bc, GYARADOS, 15
 	call GivePokemon
-	jr nc, .party_full
+	jr nc, .party_full  ; Si el equipo está lleno, mostrar mensaje
+
+	; Marcar el evento como completado
+	SetEvent EVENT_GOT_ROUTE12_GYARADOS  
+
+	; Ocultar la Poké Ball del mapa
 	ld a, HS_ROUTE12_GATE1F_POKEMON_GIFT
 	ld [wMissableObjectIndex], a
 	predef HideObject
-	db ROUTE_12_GATE_1F, ROUTE12_GATE1F_POKEMON_POKEBALL, HIDE
+	
 .party_full
 	jp TextScriptEnd
+
+.already_got_it
+	jp TextScriptEnd  ; Si ya se recogió, no hacer nada
