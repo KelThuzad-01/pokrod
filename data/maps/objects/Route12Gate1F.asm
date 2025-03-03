03e6a7c6716f26e3f82bc08ad82c8e3d165ee08c
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-03-03 14:35:00 +0100

    Update Route12Gate1F.asm

--- data/maps/objects/Route12Gate1F.asm
+++ data/maps/objects/Route12Gate1F.asm
@@ -16,20 +16,6 @@ Route12Gate1F_Object:
 
 	def_object_events
 	object_event  1,  3, SPRITE_GUARD, STAY, NONE, TEXT_ROUTE12GATE1F_GUARD
-	 ; Verificar si el jugador ya ha recibido a Gyarados
-	    CheckEvent EVENT_GOT_ROUTE12_POKEMON
-	    jr nz, .skip_pokeball  ; Si ya lo tiene, no crear la Poké Ball
-	
-	    ; Si aún no tiene el Pokémon, agregar la Poké Ball dinámicamente
-	    ld a, 7   ; X posición
-	    ld b, 5   ; Y posición
-	    ld c, SPRITE_POKE_BALL
-	    ld d, STAY
-	    ld e, NONE
-	    ld hl, TEXT_ROUTE12_GATE1F_POKEMON_POKEBALL
-	    call PlaceObject   ; Crear la Poké Ball
-	
-	.skip_pokeball
-	    ret
+	object_event  7,  4, SPRITE_POKE_BALL, STAY, NONE, TEXT_ROUTE12_GATE1F_POKEMON_POKEBALL
 
 	def_warps_to ROUTE_12_GATE_1F
