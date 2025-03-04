2b6bb888d321cfb10c391ec2a58cea189beb45a7
KelThuzad-01 <alejandroberdun01@gmail.com>
2025-03-04 20:04:30 +0100

    Update Route12Gate1F.asm

--- scripts/Route12Gate1F.asm
+++ scripts/Route12Gate1F.asm
@@ -12,7 +12,7 @@ Route12Gate1FGuardText:
 	text_far _Route12Gate1FGuardText
 	text_end
 	call PrintText
-	farcall HealParty
+	call HealParty
 	call GBFadeOutToWhite
 	call Delay3
 	call GBFadeInFromWhite
